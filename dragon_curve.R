c6d5a8c67d8cdc57429dc61f31b15c8ff6261afd
BastiHz <41594266+BastiHz@users.noreply.github.com>
2018-09-01 15:38:53 +0200

    Made 0 a forbidden value for the order of the dragon curve because that would be just a straight line and I don't plan to implement support for that special case.

--- dragon_curve.R
+++ dragon_curve.R
@@ -11,16 +11,15 @@ generate_turns <- function(order, limit = 20) {
   stopifnot(exprs = {
     is.numeric(order)
     length(order) == 1
-    order >= 0
+    order > 0
   })
   if (order > limit) {
     stop("The order exceeds the limit and the resulting vector would probably be very big in ",
          "memory. This is because the length is 2 ^ order - 1. Change the value of \"limit\" ",
          "to get higher order dragon curves.")
   }
-  if (order == 0) return(numeric())
+  if (order == 1) return(1)
   curve <- 1
-  if (order == 1) return(curve)
   for (i in 2:order) {
     middle_index <- ceiling(length(curve) / 2)
     curve_2 <- curve
@@ -31,14 +30,14 @@ generate_turns <- function(order, limit = 20) {
 }
 
 
-# These should generate return errors:
+# These should generate errors:
 generate_turns(c(2, 3))
 generate_turns("3")
 generate_turns(-1)
+generate_turns(0)
 generate_turns(100)
 
 # These should work:
-generate_turns(0)
 generate_turns(1)
 generate_turns(2)
 generate_turns(3)

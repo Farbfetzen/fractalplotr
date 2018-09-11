ca81029e32c093366e02f2fb1e07d1c72c764fcb
BastiHz <41594266+BastiHz@users.noreply.github.com>
2018-09-11 23:21:06 +0200

    Functions for creating a complex matrix and rotating and flipping matrices.

---
+++ R/matrix_manipulation.R
@@ -0,0 +1,30 @@
+rotate_matrix <- function(m, direction, times) {
+  stopifnot(exprs = {
+    class(m) == "matrix"
+    direction %in% c("left", "right", "clockwise", "anticlockwise")
+    times > 0
+  })
+  for (i in seq_len(times)) {
+    if (direction %in% c("right", "clockwise")) {
+      m <- t(m[nrow(m):1, ])
+    } else {
+      m <- t(m[, ncol(m):1])
+    }
+  }
+  m
+}
+
+
+flip_matrix <- function(m, direction) {
+  stopifnot(exprs = {
+    class(m) == "matrix"
+    direction %in% c("horizontal", "vertical")
+  })
+  if (direction == "horizontal") {
+    m <- m[nrow(m):1, ]
+  } else {
+    m <- m[, ncol(m):1]
+  }
+  m
+}
+

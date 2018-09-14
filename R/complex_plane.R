83e8f9c5d39690a53793c2a6ad4fff2761390820
BastiHz <41594266+BastiHz@users.noreply.github.com>
2018-09-14 20:56:03 +0200

    Added license notice to source.

--- R/complex_plane.R
+++ R/complex_plane.R
@@ -1,3 +1,20 @@
+# fractalplotr - Plot Beautiful Fractals with R
+# Copyright (C) 2018 Sebastian Henz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses.
+
+
 make_complex_plane <- function(width, height, re_min, re_max, im_min, im_max) {
   x <- rep(seq(re_min, re_max, length.out = width), height)
   y <- rep(seq(im_max, im_min, length.out = height), each = width)

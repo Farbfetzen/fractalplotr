6498ee311030020edca9e5f268de21c1b95c74d7
BastiHz <41594266+BastiHz@users.noreply.github.com>
2018-09-23 18:48:07 +0200

    Added scribbels.R to .Rbuildignore.

--- R/scribbels.R
+++ R/scribbels.R
@@ -16,3 +16,46 @@
 
 
 # This script is for scribbels, notes, playing around with ideas.
+
+
+# w <- 1500
+# h <- 1000
+# threshold <- 100
+# max_iterations <- 128
+#
+# system.time({
+#   m <- mandelbrot(w, h, threshold = threshold, color_mode = "continuous")
+# })
+# # user  system elapsed
+# # 7.79    0.86    8.75
+#
+# plot_mandelbrot(m)
+#
+# system.time({
+#   res <- mandelbrot(w, h, threshold = threshold, color_mode = "continuous")
+# })
+#
+# color_function <- colorRampPalette(c(
+#   "navy", "white", "dark orange", "darkred", "black"
+# ))
+#
+# system.time({
+#   m_c <- mandelbrot_color_continuous(color_function, n_steps = res$n_steps,
+#                                      z = res$z, max_iterations = max_iterations)
+# })
+# # user  system elapsed
+# # 5.02    0.14    5.17
+#
+# # user  system elapsed
+# # 3.53    0.11    3.66
+#
+#
+# plot_mandelbrot(m_c)
+#
+# foo <- matrix(1:4, nrow = 2, byrow = TRUE)
+# foo
+# foo < 3
+# bar <- matrix(c(1, 3, 2, 5), nrow = 2, byrow = TRUE)
+# bar
+# pmin(foo, bar)
+# pmax(foo, bar)

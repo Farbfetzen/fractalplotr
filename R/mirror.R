db391fd54eacb9439e4277a51c048057a77f5799
bastihz <41594266+BastiHz@users.noreply.github.com>
2020-02-23 11:52:35 +0100

    Fix license, formatting, minor stuff closes #39

--- R/mirror.R
+++ R/mirror.R
@@ -1,3 +1,19 @@
+# Copyright (C) 2020 Sebastian Henz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses/.
+
+
 #' Mirror Fractals
 #'
 #' Mirror fractals horizontally or vertically.
@@ -45,10 +61,7 @@ mirror.default <- function(fractal,
 mirror.dragon_curve <- function(fractal,
                                 direction = c("horizontal", "vertical")) {
     direction <- match.arg(direction)
-    axis <- switch (direction,
-        horizontal = "x",
-        vertical = "y"
-    )
+    axis <- switch(direction, horizontal = "x", vertical = "y")
     fractal[, axis] <- fractal[, axis] * -1
     fractal
 }
--- tests/testthat/test_complex_plane.R
+++ tests/testthat/test_complex_plane.R
@@ -1,14 +1,27 @@
-context("complex plane")
+# Copyright (C) 2020 Sebastian Henz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses/.
 
 
 test_that("complex plane is correctly created", {
     m1 <- matrix(
-        c(-1+4i, 0+4i, 1+4i, 2+4i,
-          -1+3i, 0+3i, 1+3i, 2+3i,
-          -1+2i, 0+2i, 1+2i, 2+2i),
+        c(-1 + 4i, 0 + 4i, 1 + 4i, 2 + 4i,
+          -1 + 3i, 0 + 3i, 1 + 3i, 2 + 3i,
+          -1 + 2i, 0 + 2i, 1 + 2i, 2 + 2i),
         nrow = 3,
         byrow = TRUE
     )
-    m2 <- make_complex_plane(4, 3, 3, 2, 0.5+3i)
+    m2 <- make_complex_plane(4, 3, 3, 2, 0.5 + 3i)
     expect_identical(m1, m2)
 })

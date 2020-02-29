1c393bebc27b2f2e7f64ae804ddf9b359ea1807f
bastihz <41594266+BastiHz@users.noreply.github.com>
2020-02-29 15:31:56 +0100

    Improve the performance of sandpile()

--- R/sandpile.R
+++ R/sandpile.R
@@ -17,7 +17,7 @@
 # TODO: Add an option to use 8 neighbors per cell. Remember that this changes
 # the limit n to 8. Adjust the documentation accordingly. The color vector must
 # be of length 8. Can I use the same symmetry speed improvements as with n = 4?
-# TODO: Improve the speed of this function. See issue #14.
+
 
 #' Sandpile
 #'
@@ -26,10 +26,10 @@
 #' @param n The number of grains dropped in the center.
 #' @param colors A vector of length 4 or \code{NULL}. Specifies the colors to be
 #'   allocated to the values \code{0:4}. If \code{NULL}, the number of grains
-#'   per cell are returned instead.
+#'   per cell are returned instead. Defaults to a grey palette.
 #'
 #' @return A matrix, either containing colors or the number of grains per cell,
-#'   depending on the argument \code{colors}.
+#'   depending on the value of \code{colors}.
 #'
 #' @references \url{https://en.wikipedia.org/wiki/Abelian_sandpile_model}
 #'
@@ -38,23 +38,21 @@
 #' plot(s)
 #'
 #' @export
-sandpile <- function(n, colors = c("white", "lightgray", "darkgray", "black")) {
+sandpile <- function(n, colors = grey.colors(4, start = 1, end = 0)) {
     stopifnot(n > 0)
     n <- as.integer(n)
     sidelength <- 11L
-    size <- sidelength * sidelength
-    right_border <- seq(size - sidelength + 1L, size)
-    almost_right_border <- min(right_border) - 1L
-    far_right <- right_border + sidelength
-    min_far_right <- min(far_right)
-    bottom_border <- seq(sidelength, size, sidelength)
-    far_bottom <- bottom_border + 1L
+    increase_by <- 10L  # must be an even number to keep the sidelength odd
     pile <- matrix(0L, sidelength, sidelength)
     pile[sidelength, sidelength] <- n
-    increase_by <- 10L  # must be an even number
-    to_topple <- which(pile > 3L)
-    while (length(to_topple) > 0L) {
-        if (any(to_topple <= sidelength)) {
+    size <- length(pile)
+    almost_right_border <- size - sidelength
+    right_border <- seq(almost_right_border + 1L, size)
+    bottom_border <- seq(sidelength, size, sidelength)
+    far_bottom <- bottom_border + 1L
+    toppling <- which(pile > 3L)
+    while (length(toppling) > 0L) {
+        if (any(toppling <= sidelength)) {
             # The sand in the first column of the matrix is about to topple.
             # Increase the size of the pile so that the grains don't spill out.
             pile <- rbind(matrix(0L, increase_by, sidelength), pile)
@@ -63,15 +61,31 @@ sandpile <- function(n, colors = c("white", "lightgray", "darkgray", "black")) {
 
             size <- length(pile)
             right_border <- seq(size - sidelength + 1L, size)
-            almost_right_border <- min(right_border) - 1L
-            far_right <- right_border + sidelength
-            min_far_right <- min(far_right)
+            almost_right_border <- size - sidelength
             bottom_border <- seq(sidelength, size, sidelength)
             far_bottom <- bottom_border + 1L
-            to_topple <- which(pile > 3L)
+            toppling <- which(pile > 3L)
         }
 
-        pile[to_topple] <- pile[to_topple] - 4L
+        k <- pile[toppling] %/% 4L
+        pile[toppling] <- pile[toppling] %% 4L
+
+
+        top_neighbors <- toppling - 1L
+        left_neighbors <- toppling - sidelength
+        right_neighbors <- toppling + sidelength
+        right_filter <- right_neighbors <= size
+        right_neighbors <- right_neighbors[right_filter]
+        k_right <- k[right_filter]
+        bottom_neighbors <- toppling + 1L
+        bottom_filter <- !bottom_neighbors %in% far_bottom
+        bottom_neighbors <- bottom_neighbors[bottom_filter]
+        k_bottom <- k[bottom_filter]
+
+        pile[top_neighbors] <- pile[top_neighbors] + k
+        pile[left_neighbors] <- pile[left_neighbors] + k
+        pile[right_neighbors] <- pile[right_neighbors] + k_right
+        pile[bottom_neighbors] <- pile[bottom_neighbors] + k_bottom
 
         # To use the symmetry I have to handle the borders in a special way.
         # Because the right border and the bottom border, which are the middle
@@ -80,31 +94,19 @@ sandpile <- function(n, colors = c("white", "lightgray", "darkgray", "black")) {
         # from the left and then I add sand again because it would also flow
         # into it from the right. Same with the bottom border but for this I
         # just copy the right border.
-
-        # FIXME: Come back later and think of a better way to do this.
-        # Can't I define that beforehand somehow and then just look it up?
-        top_neighbors <- to_topple - 1L
-        left_neighbors <- to_topple - sidelength
-        right_neighbors <- to_topple + sidelength
-        right_neighbors <- right_neighbors[right_neighbors < min_far_right]
-        bottom_neighbors <- to_topple + 1L
-        bottom_neighbors <- bottom_neighbors[!bottom_neighbors %in% far_bottom]
-
-        pile[top_neighbors] <- pile[top_neighbors] + 1L
-        pile[left_neighbors] <- pile[left_neighbors] + 1L
-        pile[right_neighbors] <- pile[right_neighbors] + 1L
-        pile[bottom_neighbors] <- pile[bottom_neighbors] + 1L
-
-        right_border_again <- right_neighbors[right_neighbors > almost_right_border]
-        if (length(right_border_again) > 0L) {
-            pile[right_border_again] <- pile[right_border_again] + 1L
+        right_again_filter <- right_neighbors > almost_right_border
+        if (any(right_again_filter)) {
+            right_border_again <- right_neighbors[right_again_filter]
+            k_right_again <- k_right[right_again_filter]
+            pile[right_border_again] <- pile[right_border_again] + k_right_again
             pile[bottom_border] <- pile[right_border]
             if (size %in% right_neighbors) {
-                pile[size] <- pile[size] + 1L
+                # Also add one to the center
+                pile[size] <- pile[size] + k_right_again[length(k_right_again)]
             }
         }
 
-        to_topple <- which(pile > 3L)
+        toppling <- which(pile > 3L)
     }
 
     # trim the edges solely consisting of zeroes:
